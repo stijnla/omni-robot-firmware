ef88317c3bec3ba0ab258c0ce6b32606d8b86d36
Stijn Lafontaine <stijn.lafontaine@ziggo.nl>
2025-03-30 21:46:50 +0200

    added XOR checksum to msg. Added angle measurement to msg. IMU bias is all 0, to be determined by cpu now. Gyro is more precise.

--- src/main.cpp
+++ src/main.cpp
@@ -36,16 +36,20 @@ VelocityControl velocity_control_right(dc_motor_right, hal_sensor_right, 50.0, 1
 Command current_command;
 
 // IMU calibration data
-double acceleration_calibration[3] = {0.02, 0.02, 0.01};
-double gyro_calibration[3] = {-1.38, 1.90, 1.77};
-calData calib = { 1,};  //Calibration data
-AccelData accelData;    //Sensor data
+double acceleration_calibration[3] = {0.00, 0.00, 0.00};
+double gyro_calibration[3] = {0.00, 0.00, 0.00};
+calData calib = {
+    1,
+}; // Calibration data
+
+AccelData accelData; // Sensor data
 GyroData gyroData;
 
 // Function to switch to the desired bus on the multiplexer
-void TCA9548A(uint8_t bus){
-  Wire.beginTransmission(0x70);  // TCA9548A address is 0x70
-  Wire.write(1 << bus);          // send byte to select bus
+void TCA9548A(uint8_t bus)
+{
+  Wire.beginTransmission(0x70); // TCA9548A address is 0x70
+  Wire.write(1 << bus);         // send byte to select bus
   Wire.endTransmission();
 }
 
@@ -57,24 +61,34 @@ void setup()
   Wire.setClock(1000000);
   // Initialize multiplexer, do not continue if it fails
   if (MP.begin() == false)
-  { 
-    while (true){
+  {
+    while (true)
+    {
       Serial.println("Could not connect to TCA9548 multiplexer.");
     }
   }
-  //Initialize IMU, do not continue if it fails
+  // Initialize IMU, do not continue if it fails
   int err = IMU.init(calib, IMU_ADDRESS);
-  if (err != 0) {
-    while (true) {
+  if (err != 0)
+  {
+    while (true)
+    {
       Serial.print("Error initializing IMU: ");
       Serial.println(err);
     }
   }
-  if (true){ // false if calibrating not necessary
-    Serial.println("Calibrating... Keep IMU level.");
-    delay(5000);
-    IMU.calibrateAccelGyro(&calib);
-    Serial.println("Calibration done!");
+  if (true)
+  { // false if calibrating not necessary
+    // Serial.println("Calibrating... Keep IMU level.");
+    // delay(5000);
+    // IMU.calibrateAccelGyro(&calib);
+    // Serial.println("Calibration done!");
+    calib.accelBias[0] = acceleration_calibration[0];
+    calib.accelBias[1] = acceleration_calibration[1];
+    calib.accelBias[2] = acceleration_calibration[2];
+    calib.gyroBias[0] = gyro_calibration[0];
+    calib.gyroBias[1] = gyro_calibration[1];
+    calib.gyroBias[2] = gyro_calibration[2];
     Serial.println("Accel biases X/Y/Z: ");
     Serial.print(calib.accelBias[0]);
     Serial.print(", ");
@@ -87,16 +101,16 @@ void setup()
     Serial.print(calib.gyroBias[1]);
     Serial.print(", ");
     Serial.println(calib.gyroBias[2]);
-  
-  delay(5000);
-  IMU.init(calib, IMU_ADDRESS);
+    IMU.init(calib, IMU_ADDRESS);
   }
 
-  //ranges: 250, 500, 1000, 2000 degrees / s for gyro, 2, 4, 8, 16 g for accel
-  err = IMU.setGyroRange(500);      //USE THESE TO SET THE gyro RANGE, IF AN INVALID RANGE IS SET IT WILL RETURN -1
-  err = IMU.setAccelRange(2);       //THESE TWO SET THE accel RANGE TO ±500 DPS AND THE ACCELEROMETER RANGE TO ±2g 
-  if (err != 0) {
-    while (true) {
+  // ranges: 250, 500, 1000, 2000 degrees / s for gyro, 2, 4, 8, 16 g for accel
+  err = IMU.setGyroRange(250); // USE THESE TO SET THE gyro RANGE, IF AN INVALID RANGE IS SET IT WILL RETURN -1
+  err = IMU.setAccelRange(2);  // THESE TWO SET THE accel RANGE TO ±500 DPS AND THE ACCELEROMETER RANGE TO ±2g
+  if (err != 0)
+  {
+    while (true)
+    {
       Serial.print("Error Setting range: ");
       Serial.println(err);
     }
@@ -104,7 +118,7 @@ void setup()
 }
 
 void loop()
-{  
+{
   current_command.readMessage(); // read incoming message
   int desired_speed_left = current_command.getDesiredSpeedLeft();
   int desired_speed_right = current_command.getDesiredSpeedRight();
@@ -115,34 +129,23 @@ void loop()
   TCA9548A(2);
   VelocityData s2 = velocity_control_right.setSpeed(desired_speed_right);
 
-  //Read IMU data
+  // Read IMU data
   IMU.update();
   IMU.getAccel(&accelData);
   IMU.getGyro(&gyroData);
-  
-  //Sent data back to master
-  Serial.print("!");
-  Serial.print(accelData.accelX);
-  Serial.print(",");
-  Serial.print(accelData.accelY);
-  Serial.print(",");
-  Serial.print(accelData.accelZ);
-  Serial.print(",");
-  Serial.print(gyroData.gyroX);
-  Serial.print(",");
-  Serial.print(gyroData.gyroY);
-  Serial.print(",");
-  Serial.print(gyroData.gyroZ);
-  Serial.print(",");
-  Serial.print(IMU.getTemp());
-  Serial.print(",");
-  Serial.print(s1.current_speed);
-  Serial.print(",");
-  Serial.print(s1.control_signal);
-  Serial.print(",");
-  Serial.print(s2.current_speed);
-  Serial.print(","); 
-  Serial.print(s2.control_signal);
-  Serial.println();
-}
 
+  // Sent data back to master
+  String data = "!" + String(accelData.accelX) + "," + String(accelData.accelY) + "," + String(accelData.accelZ) + "," +
+                String(gyroData.gyroX) + "," + String(gyroData.gyroY) + "," + String(gyroData.gyroZ) + "," +
+                String(IMU.getTemp()) + "," + String(s1.current_angle) + "," + String(s1.current_speed) + "," +
+                String(s1.control_signal) + "," + String(s2.current_angle) + "," + String(s2.current_speed) + "," +
+                String(s2.control_signal);
+  // Calculate checksum
+  uint8_t checksum = 0;
+  for (size_t i = 0; i < data.length(); i++)
+  {
+    checksum ^= data[i];
+  }
+  data += "," + String(checksum);
+  Serial.println(data);
+}
